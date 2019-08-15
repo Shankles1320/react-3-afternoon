5ba3787dce2c1321be2d133941380634188b285c
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-08-15 15:03:35 -0700

    reactday4afternoon

--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -21,6 +21,11 @@ export default class Compose extends Component {
   }
 
   createPost() {
+    const { text } = this.state;
+    const{ createPostFn } = this.props;
+
+    createPostFn( text );
+    this.setState({ text: '' });
 
   }
 
@@ -40,7 +45,8 @@ export default class Compose extends Component {
           <input className="Compose__input"
                  placeholder="What's on your mind?"
                  value={ text }
-                 onChange={ ( e ) => this.updateText( e.target.value ) } />
+                 onChange={ ( e ) => this.updateText( e.target.value ) } 
+                 />
 
         </div>
 
